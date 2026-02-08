e07f2398b06c0b9d91a7f9873d50995961e5c156
kaustubhshekokar <kaustubhshekokar@gmail.com>
2026-02-08 23:21:44 +0530

    self and init

--- p85_objects&classes.py
+++ p85_objects&classes.py
@@ -4,4 +4,9 @@ class Instructor:
 instructor_1=Instructor()
 print(type(instructor_1))
 
+class CarDesign:
+    brand="Tata"
+    model="cierra"
 
+mycar=CarDesign()
+print(mycar)    
---
+++ p86_init&self.py
@@ -0,0 +1,14 @@
+class Instructor:
+    def __init__(self,name,address,followers):
+        self.name=name
+        self.address=address
+        self.followers=0
+
+
+Ins1=Instructor("Kaustubh","Akola",0)
+print(Ins1.name)
+print(Ins1.address)
+print(Ins1.followers)
+
+
+        
